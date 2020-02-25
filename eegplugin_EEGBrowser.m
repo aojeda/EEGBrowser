54e4e35eb8e51b0a2327d148160132b387694acb
Arnaud Delorme <arnodelorme@gmail.com>
2020-02-25 07:49:45 -0600

    Update eegplugin_EEGBrowser.m
    Fix crash due to modified EEGLAB menu.

--- eegplugin_EEGBrowser.m
+++ eegplugin_EEGBrowser.m
@@ -5,14 +5,11 @@
 % Author: Alejandro Ojeda, Neural Engineering and Translation Lab, 
 % Department of Psychiatry, UC San Diego, 2019
 
-function eegplugin_EEGBrowser(fig,try_strings, catch_strings)
+function v = eegplugin_EEGBrowser(fig,try_strings, catch_strings)
+
+v = '1.1';
 p = fileparts(which('eegplugin_EEGBrowser'));
 addpath(p);
 hPlot = findobj(fig,'Label','Plot');
-uimenu( hPlot, 'label', 'Channel data (EEG Browser)', 'callback','pop_eegbrowser(EEG);');
-uimenu( hPlot, 'label', 'Component activations (EEG Browser)', 'callback','pop_eegbrowser(EEG,0);');
-labels = get(hPlot.Children,'label');
-n = length(hPlot.Children);
-ind  = [find(ismember(labels,'Channel data (scroll)')) find(ismember(labels,'Component activations (scroll)'))];
-idx = [3:n-ind(2)+2 1 ind(2):ind(1)-1 2 ind(1):n];
-hPlot.Children = hPlot.Children(idx);
+uimenu( hPlot, 'label', 'Channel data (EEG Browser)', 'callback','pop_eegbrowser(EEG);', 'position', 3);
+uimenu( hPlot, 'label', 'Component activations (EEG Browser)', 'callback','pop_eegbrowser(EEG,0);', 'position', 10);
